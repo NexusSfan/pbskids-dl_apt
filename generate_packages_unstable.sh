6bcba6d01aebea190d402bf8e76482ab27208541
NexusSfan <nexussfan@duck.com>
2024-07-27 21:38:58 +0000

    Sonic Wave

--- generate_packages_unstable.sh
+++ generate_packages_unstable.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-rm dists/unstable/main/binary-all/Packages
-rm dists/unstable/main/binary-all/Packages.gz
 dpkg-scanpackages --arch all pool/ > dists/unstable/main/binary-all/Packages
 cat dists/unstable/main/binary-all/Packages | gzip -9 > dists/unstable/main/binary-all/Packages.gz
