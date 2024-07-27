d57b37861c7729f1343d011e95ed9a23bb7a8c9f
NexusSfan <nexussfan@duck.com>
2024-07-27 14:11:41 -0700

    ???

--- generate-packages.sh
+++ generate-packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dpkg-scanpackages --arch all pool/ > dists/stable/main/binary-all/Packages
 cat dists/stable/main/binary-all/Packages | gzip -9 > dists/stable/main/binary-all/Packages.gz
+dpkg-scanpackages --arch all pool/ > dists/unstable/main/binary-all/Packages
+cat dists/unstable/main/binary-all/Packages | gzip -9 > dists/unstable/main/binary-all/Packages.gz
