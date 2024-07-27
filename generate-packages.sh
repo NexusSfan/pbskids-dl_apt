e0985f4754c4b5779357cb3e91d4120c2d3a44f7
NexusSfan <nexussfan@duck.com>
2024-07-27 14:48:56 -0700

    Bruh Momento

--- generate-packages.sh
+++ generate-packages.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dpkg-scanpackages --arch all pool/ > dists/stable/main/binary-all/Packages
+dpkg-scanpackages --arch all pool/main/stable > dists/stable/main/binary-all/Packages
 cat dists/stable/main/binary-all/Packages | gzip -9 > dists/stable/main/binary-all/Packages.gz
--- generate_packages_unstable.sh
+++ generate_packages_unstable.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-dpkg-scanpackages --arch all pool/ > dists/unstable/main/binary-all/Packages
+dpkg-scanpackages --arch all pool/main/unstable > dists/unstable/main/binary-all/Packages
 cat dists/unstable/main/binary-all/Packages | gzip -9 > dists/unstable/main/binary-all/Packages.gz
