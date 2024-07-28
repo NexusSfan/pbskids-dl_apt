852f238d18b5425802e51f1fae1c90ad63fa7ca2
NexusSfan <nexussfan@duck.com>
2024-07-28 13:38:11 -0700

    Add pbskids-dl.rs

--- generate-packages-unstable.sh
+++ generate-packages-unstable.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dpkg-scanpackages --arch all pool/main/unstable > dists/unstable/main/binary-all/Packages
 cat dists/unstable/main/binary-all/Packages | gzip -9 > dists/unstable/main/binary-all/Packages.gz
+dpkg-scanpackages --arch amd64 pool/main/unstable > dists/unstable/main/binary-amd64/Packages
+cat dists/unstable/main/binary-amd64/Packages | gzip -9 > dists/unstable/main/binary-amd64/Packages.gz
--- generate-packages.sh
+++ generate-packages.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 dpkg-scanpackages --arch all pool/main/stable > dists/stable/main/binary-all/Packages
 cat dists/stable/main/binary-all/Packages | gzip -9 > dists/stable/main/binary-all/Packages.gz
+dpkg-scanpackages --arch amd64 pool/main/stable > dists/stable/main/binary-amd64/Packages
+cat dists/stable/main/binary-amd64/Packages | gzip -9 > dists/stable/main/binary-amd64/Packages.gz
